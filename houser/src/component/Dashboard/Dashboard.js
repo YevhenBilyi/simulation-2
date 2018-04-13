79b855dba1fb9db9861a5787da6d8cc81078e476
YevhenBilyi <leugenewhitel@gmail.com>
2018-04-13 12:13:15 -0600

    done

--- houser/src/component/Dashboard/Dashboard.js
+++ houser/src/component/Dashboard/Dashboard.js
@@ -10,16 +10,23 @@ class Dashboard extends Component {
     this.state={
       list:[]
     }
+    this.componentDidMount=this.componentDidMount.bind(this);
+    this.deleteHouse=this.deleteHouse.bind(this);
   }
   componentDidMount(){
     axios.get('/api/houses').then(res=>{
       this.setState({list:res.data})
     }).catch(error=>console.log('Failed at getting data:'+error))
   }
+  deleteHouse(i){
+    axios.delete(`/api/house/${i}`).then(res=>{
+      this.componentDidMount()
+    }).catch(err=>console.log("cannot delete house:"+err))
+  }
   render() {
     let list=this.state.list.map((house,i)=>{
       return ( <House key={i} name={house.name} address={house.address} city={house.city} 
-        state={house.state} zip={house.zip} id={house.id}/>)
+        state={house.state} zip={house.zip} id={house.id} delete={this.deleteHouse}/>)
     })
     return (
       <div className="Dashboard">
